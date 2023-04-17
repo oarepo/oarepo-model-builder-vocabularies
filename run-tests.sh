5cc7acaaf64c35e5bb4378bb6154b806e012948c
Mirek Simek <miroslav.simek@gmail.com>
2023-04-17 21:13:12 +0200

    trying tests

--- run-tests.sh
+++ run-tests.sh
@@ -10,9 +10,13 @@ python3 -m venv .venv
 
 BUILDER=.venv/bin/oarepo-compile-model
 
-rm -rf tests/article
+if [ -d built_tests ] ; then
+    rm -rf built_tests
+fi
 
-${BUILDER} tests/article.yaml --output-directory tests/article -vvv
+mkdir built_tests
+
+${BUILDER} tests/article.yaml --output-directory built_tests/article -vvv
 
 rm -rf .venv-tests
 python3 -m venv .venv-tests
@@ -20,9 +24,10 @@ python3 -m venv .venv-tests
 source .venv-tests/bin/activate
 
 pip install -U setuptools pip wheel
+pip install cairocffi
 pip install pyyaml opensearch-dsl 
 pip install oarepo
-pip install tests/article
+pip install built_tests/article
 pip install pytest-invenio
 pip install 'oarepo-runtime>=1.1.5'
 pip install 'oarepo-vocabularies>=1.0.4'
